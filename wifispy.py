7eb9f4a833e3913e17a2dd43f2a965ae3c4b0216
Max Harlow <max.harlow@os.uk>
2016-03-31 15:55:58 +0100

    Write sources to CSV, other improvements

--- wifispy.py
+++ wifispy.py
@@ -2,7 +2,9 @@ import sys
 import os
 import random
 import time
-import multiprocessing
+import datetime
+import threading
+import csv
 import pcapy
 import dpkt
 
@@ -23,23 +25,44 @@ channels = [
     # 36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 149, 153, 157, 161 # 5GHz
 ]
 
+store = {}
+
 def start():
     os.system(enable_monitor)
     rotator(channels, change_channel)
+    writer()
     try: sniff(interface)
-    except KeyboardInterrupt as e: sys.exit(e)
+    except KeyboardInterrupt: sys.exit()
+    except SystemError: sys.exit()
     finally: os.system(disable_monitor)
 
 def rotator(channels, change_channel):
-    def hop():
-        while True:
-            try:
+    def rotate():
+        try:
+            while True:
                 channel = random.choice(channels)
-                print('\nChanging to channel ' + str(channel) + '...\n')
+                print('\nChanging to channel ' + str(channel) + '\n')
                 os.system(change_channel.format(channel))
                 time.sleep(1) # seconds
-            except BaseException as e: sys.exit(e)
-    multiprocessing.Process(target=hop).start()
+        except BaseException: sys.exit()
+    threading.Thread(target=rotate).start()
+
+def writer():
+    def write():
+        try:
+            while True:
+                print('\nWriting to file...\n')
+                with open('wifispy.csv', 'w') as csvFile:
+                    writer = csv.writer(csvFile)
+                    writer.writerow(['address', 'firstSeen', 'lastSeen'])
+                    writer.writerows([[address, entry['firstSeen'], entry['lastSeen']] for address, entry in store.items()])
+                time.sleep(1) # seconds
+        except BaseException as e: sys.exit(e)
+    threading.Thread(target=write).start()
+
+def add(address, timestamp):
+    if address in store: store[address]['lastSeen'] = timestamp
+    else: store[address] = { 'firstSeen': timestamp, 'lastSeen': timestamp }
 
 def to_address(address): # decode a MAC or BSSID address
     return ':'.join('%02x' % ord(b) for b in address)
@@ -51,6 +74,7 @@ def sniff(interface):
     packets = pcapy.open_live(interface, max_packet_size, promiscuous, timeout)
     packets.setfilter('') # bpf syntax (empty string = everything)
     def loop(header, data):
+        timestamp = datetime.datetime.now().isoformat()
         try:
             packet = dpkt.radiotap.Radiotap(data)
             frame = packet.data
@@ -60,18 +84,20 @@ def sniff(interface):
                 destination_address = to_address(frame.mgmt.dst)
                 ap_address = to_address(frame.mgmt.bssid)
                 ap_name = frame.ssid.data if hasattr(frame, 'ssid') else '(n/a)'
-                print('[MANAGEMENT FRAME] ' + subtype + ' * ' + ap_name + ' * ' + source_address + ' => ' + destination_address)
+                add(source_address, timestamp)
+                print('[MANAGEMENT] ' + subtype + ' * ' + source_address + ' -> ' + destination_address + ' * ' + ap_name)
             elif frame.type == dpkt.ieee80211.CTL_TYPE:
                 subtype = str(frame.subtype)
-                print('[CONTROL FRAME] ' + subtype)
+                print('[CONTROL   ] ' + subtype)
             elif frame.type == dpkt.ieee80211.DATA_TYPE:
                 subtype = str(frame.subtype)
                 source_address = to_address(frame.data_frame.src)
                 destination_address = to_address(frame.data_frame.dst)
                 ap_address = to_address(frame.data_frame.bssid) if hasattr(frame.data_frame, 'bssid') else '(n/a)'
-                print('[DATA FRAME] ' + subtype + ' * ' + ap_address + ' * ' + source_address + ' => ' + destination_address)
+                add(source_address, timestamp)
+                print('[DATA      ] ' + subtype + ' * ' + source_address + ' -> ' + destination_address + ' * ' + ap_address)
         except:
-            print('[COULD NOT PARSE PACKET]')
+            print('[ERROR PARSING PACKET]')
     packets.loop(-1, loop)
 
 start()
