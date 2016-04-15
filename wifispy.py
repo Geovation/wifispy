beafcf268b72de034a905d55b1bcb1f2c9ab1a70
Max Harlow <max.harlow@os.uk>
2016-04-15 15:26:18 +0100

    Human-readable names for subtypes

--- wifispy.py
+++ wifispy.py
@@ -27,6 +27,49 @@ channels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] # 2.4GHz only
 
 queue = multiprocessing.Queue()
 
+subtypes_management = {
+    0: 'association-request',
+    1: 'association-response',
+    2: 'reassociation-request',
+    3: 'reassociation-response',
+    4: 'probe-request',
+    5: 'probe-response',
+    8: 'beacon',
+    9: 'announcement-traffic-indication-message',
+    10: 'disassociation',
+    11: 'authentication',
+    12: 'deauthentication',
+    13: 'action'
+}
+
+subtypes_control = {
+    8: 'block-acknowledgement-request',
+    9: 'block-acknowledgement',
+    10: 'power-save-poll',
+    11: 'request-to-send',
+    12: 'clear-to-send',
+    13: 'acknowledgement',
+    14: 'contention-free-end',
+    15: 'contention-free-end-plus-acknowledgement'
+}
+
+subtypes_data = {
+    0: 'data',
+    1: 'data-and-contention-free-acknowledgement',
+    2: 'data-and-contention-free-poll',
+    3: 'data-and-contention-free-acknowledgement-plus-poll',
+    4: 'null',
+    5: 'contention-free-acknowledgement',
+    6: 'contention-free-poll',
+    7: 'contention-free-acknowledgement-plus-poll',
+    8: 'qos-data',
+    9: 'qos-data-plus-contention-free-acknowledgement',
+    10: 'qos-data-plus-contention-free-poll',
+    11: 'qos-data-plus-contention-free-acknowledgement-plus-poll',
+    12: 'qos-null',
+    14: 'qos-contention-free-poll-empty'
+}
+
 def start():
     logging.basicConfig(filename='wifispy.log', format='%(levelname)s:%(message)s', level=logging.INFO)
     os.system(monitor_enable)
@@ -120,7 +163,7 @@ def sniff(interface):
                 record = {
                     'timestamp': timestamp,
                     'type': 'management',
-                    'subtype': str(frame.subtype),
+                    'subtype': subtypes_management[frame.subtype],
                     'strength': packet_signal,
                     'source_address': to_address(frame.mgmt.src),
                     'destination_address': to_address(frame.mgmt.dst),
@@ -132,7 +175,7 @@ def sniff(interface):
                 record = {
                     'timestamp': timestamp,
                     'type': 'control',
-                    'subtype': str(frame.subtype),
+                    'subtype': subtypes_control[frame.subtype],
                     'strength': packet_signal,
                     'source_address': '(n/a)', # not available in control packets
                     'destination_address': '(n/a)', # not available in control packets
@@ -144,7 +187,7 @@ def sniff(interface):
                 record = {
                     'timestamp': timestamp,
                     'type': 'data',
-                    'subtype': str(frame.subtype),
+                    'subtype': subtypes_data[frame.subtype],
                     'strength': packet_signal,
                     'source_address': to_address(frame.data_frame.src),
                     'destination_address': to_address(frame.data_frame.dst),
